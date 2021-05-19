f17cfaac5247817bf38fad7d854c09c4b48a4bed
Viviane Scarlatti <viviane.scarlatti@gmail.com>
2021-05-19 20:52:57 -0300

    Implementando o repository

---
+++ src/main/java/br/com/alura/escola/domain/aluno/RepositorioDeAlunos.java
@@ -0,0 +1,9 @@
+package br.com.alura.escola.domain.aluno;
+
+import java.util.List;
+
+public interface RepositorioDeAlunos {
+    void matricular(Aluno aluno);
+    Aluno buscarPorCPF(CPF cpf);
+    List<Aluno> listarTodosAlunosMatriculados();
+}
---
+++ src/main/java/br/com/alura/escola/integration/aluno/AlunosRepositoryEmMemoria.java
@@ -0,0 +1,32 @@
+package br.com.alura.escola.integration.aluno;
+
+import br.com.alura.escola.domain.aluno.Aluno;
+import br.com.alura.escola.domain.aluno.AlunoNaoEncontrado;
+import br.com.alura.escola.domain.aluno.CPF;
+import br.com.alura.escola.domain.aluno.RepositorioDeAlunos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlunosRepositoryEmMemoria implements RepositorioDeAlunos {
+
+    private  List<Aluno> matriculados = new ArrayList<>();
+
+    @Override
+    public void matricular(Aluno aluno) {
+        this.matriculados.add(aluno);
+    }
+
+    @Override
+    public Aluno buscarPorCPF(CPF cpf) {
+        return this.matriculados.stream()
+                .filter(a -> a.getCpf().equals(cpf.getNumero()))
+                .findFirst()
+                .orElseThrow(() -> new AlunoNaoEncontrado(cpf));
+    }
+
+    @Override
+    public List<Aluno> listarTodosAlunosMatriculados() {
+        return this.matriculados;
+    }
+}
